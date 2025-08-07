46557982663dc62ccf1b9abef50f1ca95891608b
Chandramohan <Senchandramohan2018@gmail.com>
2025-08-07 20:13:55 +0530

    Initial commit

---
+++ VisionAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VisionAI
+//
+//  Created by Chandramohan  on 07/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisionAI/VisionAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionAIApp.swift
+//  VisionAI
+//
+//  Created by Chandramohan  on 07/08/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
